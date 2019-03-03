78569b69122b835ea08554cd2cabb1d05b309a60
Adim <adimchiudo@gmail.com>
2019-03-03 23:47:16 +0100

    added twig and simple page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController {
+    /**
+     * @Route("/hello/{name}")
+     */
+    public function index($name){   
+        return new Response('Hello ' . $name);
+    }
+    
+    /**
+     *@Route("/simple")
+     */
+    public function simple(){
+        return new Response("This is really simple bro");
+    }
+}
